85719d86b90dd8fddc9dc2e19ab5c96965c307f6
Yuki Matsukura <matsubokkuri@gmail.com>
2025-04-30 11:05:48 +0900

    added GA

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import KeyboardDisplay from "@/components/keyboard-display"
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center bg-black p-0 overflow-hidden">
       <KeyboardDisplay />
+      <GoogleAnalytics gaId="G-WX3ZJHM72Z" />
     </main>
   )
 }
